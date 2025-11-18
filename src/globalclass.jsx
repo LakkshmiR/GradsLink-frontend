f3fda4eae3ea2038a9e13af97349092d7262352f
Lakkshmi <rrlakkshmi@gmail.com>
2025-11-19 00:39:21 +0530

    sortedjobs

--- src/globalclass.jsx
+++ src/globalclass.jsx
@@ -61,68 +61,6 @@
 
 //////////////////////////////////////////////////////////////////vercel/////////////////////////////////////////////////////////////
 //vercel
-// import "./globalclass.css";
-// import { useNavigate } from "react-router-dom";
-// import { useState } from "react";
-// import { useEffect } from "react";
-// import axios from "axios";
-// function Globalclass({ hidethis }) {
-//   const loggedinuser = localStorage.getItem("name");
-//   const navigate = useNavigate();
-//   const handleLogout = () => {
-//     localStorage.removeItem("token");
-//     localStorage.removeItem("name");
-//     localStorage.removeItem("email");
-
-//     navigate("/login");
-//     alert("You are logged out!!");
-//   };
-//   //total users
-//   const [usercount, setusercount] = useState("");
-//   useEffect(() => {
-//     axios
-//       .get("https://gradslink-25.onrender.com/totalusers")
-//       .then((result) => {
-//         console.log(result);
-//         setusercount(result.data);
-//       })
-//       .catch((err) => console.log(err));
-//   }, []);
-//   //three dots
-//   const [openmenu, setopenmenu] = useState(false);
-//   //logout
-
-//   return (
-//     <>
-//       <div className="global-styles">
-//         <div className="global-class">
-//           <h1 className="logo">GradsLink</h1>
-//           <div>
-//             {!hidethis && (
-//               <div className="user-row">
-//                 <h1 className="user">Hi, {loggedinuser}</h1>
-
-//                 <div className="three-dots" onClick={() => setopenmenu(!openmenu)}>
-//                   ⋮
-//                 </div>
-
-//                 {openmenu && (
-//                   <div className="logout-dropdown" onClick={handleLogout}>
-//                     Logout
-//                   </div>
-//                 )}
-//               </div>
-//             )}
-//             {!hidethis && <h1 className="user-count">Total Grads: {usercount}</h1>}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-// export default Globalclass;
-
-///////////////////////////////lh//////////////////////////////////////////////////////////////////////////////
 import "./globalclass.css";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
@@ -143,7 +81,7 @@ function Globalclass({ hidethis }) {
   const [usercount, setusercount] = useState("");
   useEffect(() => {
     axios
-      .get("http://localhost:3000/totalusers")
+      .get("https://gradslink-25.onrender.com/totalusers")
       .then((result) => {
         console.log(result);
         setusercount(result.data);
@@ -183,3 +121,65 @@ function Globalclass({ hidethis }) {
   );
 }
 export default Globalclass;
+
+///////////////////////////////lh//////////////////////////////////////////////////////////////////////////////
+// import "./globalclass.css";
+// import { useNavigate } from "react-router-dom";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import axios from "axios";
+// function Globalclass({ hidethis }) {
+//   const loggedinuser = localStorage.getItem("name");
+//   const navigate = useNavigate();
+//   const handleLogout = () => {
+//     localStorage.removeItem("token");
+//     localStorage.removeItem("name");
+//     localStorage.removeItem("email");
+
+//     navigate("/login");
+//     alert("You are logged out!!");
+//   };
+//   //total users
+//   const [usercount, setusercount] = useState("");
+//   useEffect(() => {
+//     axios
+//       .get("http://localhost:3000/totalusers")
+//       .then((result) => {
+//         console.log(result);
+//         setusercount(result.data);
+//       })
+//       .catch((err) => console.log(err));
+//   }, []);
+//   //three dots
+//   const [openmenu, setopenmenu] = useState(false);
+//   //logout
+
+//   return (
+//     <>
+//       <div className="global-styles">
+//         <div className="global-class">
+//           <h1 className="logo">GradsLink</h1>
+//           <div>
+//             {!hidethis && (
+//               <div className="user-row">
+//                 <h1 className="user">Hi, {loggedinuser}</h1>
+
+//                 <div className="three-dots" onClick={() => setopenmenu(!openmenu)}>
+//                   ⋮
+//                 </div>
+
+//                 {openmenu && (
+//                   <div className="logout-dropdown" onClick={handleLogout}>
+//                     Logout
+//                   </div>
+//                 )}
+//               </div>
+//             )}
+//             {!hidethis && <h1 className="user-count">Total Grads: {usercount}</h1>}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+// export default Globalclass;
