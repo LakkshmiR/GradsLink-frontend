52d7cb4d7aa94392df662d18322dfa969161b5b4
Lakkshmi <rrlakkshmi@gmail.com>
2025-11-21 15:16:50 +0530

    gg btn upd4

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ createRoot(document.getElementById("root")).render(
         google.accounts.id.initialize({
           client_id: "789816508710-pt97v4vabqar5kiea24mkhnqsbgeovhl.apps.googleusercontent.com",
           callback: handleGoogleLogin,
-          auto_select: false,
+          auto_select: false, // <---- IMPORTANT
+          cancel_on_tap_outside: false,
         });
       }}
     >
