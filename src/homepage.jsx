108c7f61ea16f0d761179f954ad5b1343f8782d2
Lakkshmi <rrlakkshmi@gmail.com>
2025-12-09 22:28:47 +0530

    loading in homepage

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -1,5 +1,6 @@
 /////////////////////////////////////////////////////////////////lh///////////////////////////////////////////////////////
 import Globalclass from "./globalclass";
+import Loading from "./loading";
 import "./jobsPage.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
@@ -56,89 +57,111 @@ function Homepage() {
     e.preventDefault(); // stop full page reload
     navigate("/login"); // navigate internally via React Router
   };
+  ///loading part
+  const [showloading, setShowloading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://gradslink-25.onrender.com/health")
+      .then((result) => {
+        console.log(result.data);
+        if (result.data.status === "ok") {
+          setShowloading(false);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
-      <div className="homepage-header">
-        <Globalclass hidethis={true} />
-        <div className="homepage-userbtn">
-          <button className="loginbtn-homepage" onClick={() => navigate("/login")}>
-            Login
-          </button>
-          <button className="loginbtn-homepage" onClick={() => navigate("/register")}>
-            signup
-          </button>
+      {showloading && <Loading />}
+      {!showloading && (
+        <div>
+          <div className="homepage-header">
+            <Globalclass hidethis={true} />
+            <div className="homepage-userbtn">
+              <button className="loginbtn-homepage" onClick={() => navigate("/login")}>
+                Login
+              </button>
+              <button className="loginbtn-homepage" onClick={() => navigate("/register")}>
+                signup
+              </button>
+            </div>
+          </div>
+          <h1 className="heading-homepage">Posted Jobs</h1>
+
+          <div className="job-elements">
+            <div className="search-container">
+              <input
+                type="text"
+                placeholder="Search role here..."
+                className="search-input"
+                onChange={(e) => setSearchtext(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div className="wrapper">
+            <table className="jb-table">
+              <tr className="jb-table-tr">
+                <th className="jb-table-th">Company</th>
+                <th className="jb-table-th">Role</th>
+                <th className="jb-table-th">Experience</th>
+                <th className="jb-table-th">Link</th>
+                <th className="jb-table-th">Posted At</th>
+              </tr>
+              <tbody>
+                {jobstoshow.map((company) => {
+                  const dateObj = new Date(company.postedAt);
+                  const indianDate = dateObj.toLocaleDateString("en-IN", {
+                    timeZone: "Asia/Kolkata",
+                  });
+                  const indianTime = dateObj.toLocaleTimeString("en-IN", {
+                    timeZone: "Asia/Kolkata",
+                  });
+                  const loggedinuser = localStorage.getItem("name");
+                  return (
+                    <tr key={company._id} className="jb-table-tr">
+                      <td data-cell="companyname" className="jb-table-td">
+                        {company.companyName}
+                      </td>
+                      <td data-cell="role" className="jb-table-td">
+                        {company.jobrole}
+                      </td>
+                      <td data-cell="experience" className="jb-table-td">
+                        {company.experience}
+                      </td>
+                      <td data-cell="link" className="jb-table-td">
+                        <span
+                          rel="noopener noreferrer"
+                          className="jobspage-link"
+                          onClick={handleClick}
+                        >
+                          Click Here
+                        </span>
+                      </td>
+                      <td data-cell="postedat" className="jb-table-td">
+                        {indianDate}
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+
+          <div className="jobspage-wholebtn">
+            <button onClick={() => navigate("/login")} className="prevnext-btn">
+              Previous
+            </button>
+            <button onClick={() => navigate("/login")} className="prevnext-btn">
+              Next
+            </button>
+            <button type="submit" className="postbtn-jobspage" onClick={() => navigate("/login")}>
+              Post +
+            </button>
+          </div>
         </div>
-      </div>
-      <h1 className="heading-homepage">Posted Jobs</h1>
-
-      <div className="job-elements">
-        <div className="search-container">
-          <input
-            type="text"
-            placeholder="Search role here..."
-            className="search-input"
-            onChange={(e) => setSearchtext(e.target.value)}
-          />
-        </div>
-      </div>
-
-      <div className="wrapper">
-        <table className="jb-table">
-          <tr className="jb-table-tr">
-            <th className="jb-table-th">Company</th>
-            <th className="jb-table-th">Role</th>
-            <th className="jb-table-th">Experience</th>
-            <th className="jb-table-th">Link</th>
-            <th className="jb-table-th">Posted At</th>
-          </tr>
-          <tbody>
-            {jobstoshow.map((company) => {
-              const dateObj = new Date(company.postedAt);
-              const indianDate = dateObj.toLocaleDateString("en-IN", {
-                timeZone: "Asia/Kolkata",
-              });
-              const indianTime = dateObj.toLocaleTimeString("en-IN", {
-                timeZone: "Asia/Kolkata",
-              });
-              const loggedinuser = localStorage.getItem("name");
-              return (
-                <tr key={company._id} className="jb-table-tr">
-                  <td data-cell="companyname" className="jb-table-td">
-                    {company.companyName}
-                  </td>
-                  <td data-cell="role" className="jb-table-td">
-                    {company.jobrole}
-                  </td>
-                  <td data-cell="experience" className="jb-table-td">
-                    {company.experience}
-                  </td>
-                  <td data-cell="link" className="jb-table-td">
-                    <span rel="noopener noreferrer" className="jobspage-link" onClick={handleClick}>
-                      Click Here
-                    </span>
-                  </td>
-                  <td data-cell="postedat" className="jb-table-td">
-                    {indianDate}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-
-      <div className="jobspage-wholebtn">
-        <button onClick={() => navigate("/login")} className="prevnext-btn">
-          Previous
-        </button>
-        <button onClick={() => navigate("/login")} className="prevnext-btn">
-          Next
-        </button>
-        <button type="submit" className="postbtn-jobspage" onClick={() => navigate("/login")}>
-          Post +
-        </button>
-      </div>
+      )}
     </>
   );
 }
